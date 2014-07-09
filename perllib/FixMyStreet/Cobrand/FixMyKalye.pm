3b3c832f334482b0d91ef995bc87541dc25a5f60
Rommel David <rommelbenamiradavid@gmail.com>
2014-07-09 21:29:49 +0800

    Added cobrand module for FixMyKalye

---
+++ perllib/FixMyStreet/Cobrand/FixMyKalye.pm
@@ -0,0 +1,63 @@
+package FixMyStreet::Cobrand::FixMyBarangay;
+use base 'FixMyStreet::Cobrand::Default';
+
+use strict;
+use warnings;
+use mySociety::Gaze;
+
+use constant COUNCIL_ID_BROMLEY => 2482;
+
+# Special extra
+sub path_to_web_templates {
+    my $self = shift;
+    return [
+        FixMyStreet->path_to( 'templates/web/fixmykalye.com' )->stringify,
+        FixMyStreet->path_to( 'templates/web/fixmykalye' )->stringify
+    ];
+}
+
+# FixMyStreet should return all cobrands
+sub restriction {
+    return {};
+}
+
+sub admin_base_url {
+    return 'https://secure.mysociety.org/admin/bci';
+}
+
+sub title_list {
+    my $self = shift;
+    my $areas = shift;
+    my $first_area = ( values %$areas )[0];
+
+    return ["MR", "MISS", "MRS", "MS", "DR"] if $first_area->{id} eq COUNCIL_ID_BROMLEY;
+    return undef;
+}
+
+sub extra_contact_validation {
+    my $self = shift;
+    my $c = shift;
+
+    my %errors;
+
+    $c->stash->{dest} = $c->req->param('dest');
+
+    $errors{dest} = "Please enter who your message is for"
+        unless $c->req->param('dest');
+
+    if ( $c->req->param('dest') eq 'council' || $c->req->param('dest') eq 'update' ) {
+        $errors{not_for_us} = 1;
+    }
+
+    return %errors;
+}
+
+sub get_country_for_ip_address {
+    my $self = shift;
+    my $ip = shift;
+
+    return mySociety::Gaze::get_country_from_ip($ip);
+}
+
+1;
+
