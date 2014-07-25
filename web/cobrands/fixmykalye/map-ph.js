253c8dbe2a12a74a5a59e84b858bcf8ae2fdd0e4
x44x45x41x4E <d.canlas07@gmail.com>
2014-07-25 20:58:14 +0800

    Optimized map layer loader

--- web/cobrands/fixmykalye/map-ph.js
+++ web/cobrands/fixmykalye/map-ph.js
@@ -9,54 +9,18 @@ $(function(){
 });
 
 function fms_ph_onload() {
-    var area = new OpenLayers.Layer.Vector("KML", {
+
+    if ( fixmystreet.area.length == 0)
+    {
+        var area = new OpenLayers.Layer.Vector("KML", {
         strategies: [ new OpenLayers.Strategy.Fixed() ],
         protocol: new OpenLayers.Protocol.HTTP({
             url: "/cobrands/fixmykalye/ph.kml",
             format: new OpenLayers.Format.KML()
-        })
-    });
-
-    // var qc = new OpenLayers.Layer.Vector("KML", {
-    //     strategies: [ new OpenLayers.Strategy.Fixed() ],
-    //     protocol: new OpenLayers.Protocol.HTTP({
-    //         url: "/cobrands/fixmykalye/qc.kml",
-    //         format: new OpenLayers.Format.KML()
-    //     })
-    // });
-
-    // var mnl = new OpenLayers.Layer.Vector("KML", {
-    //     strategies: [ new OpenLayers.Strategy.Fixed() ],
-    //     protocol: new OpenLayers.Protocol.HTTP({
-    //         url: "/cobrands/fixmykalye/mnnl.kml",
-    //         format: new OpenLayers.Format.KML()
-    //     })
-    // });
-
-    area.styleMap.styles['default'].defaultStyle = fixmystreet.area_format;
-
-    // if ( window.location.href == "http://128.199.175.9/reports/Manila+City+Council" ) {
-    //     fixmystreet.map.addLayer(mnl)
-    // } else if ( window.location.href == "http://128.199.175.9/reports/Quezon+City+Council" ) {
-    //     fixmystreet.map.addLayer(qc)
-    // } else {
-    //     fixmystreet.map.addLayer(area);
-    // }
-
-    if ( fixmystreet.area.length > 0)
-    {
-        var area = new OpenLayers.Layer.Vector("KML", {
-            strategies: [ new OpenLayers.Strategy.Fixed() ],
-            protocol: new OpenLayers.Protocol.HTTP({
-                url: "/mapit/area/" + fixmystreet.area[i] + ".kml?simplify_tolerance=0.0001",
-                format: new OpenLayers.Format.KML()
             })
         });
+
+        area.styleMap.styles['default'].defaultStyle = fixmystreet.area_format;
+        fixmystreet.map.addLayer(area);
     }
-
-    fixmystreet.map.addLayer(area);
-
-    //area.events.register('loadend', null, function(a,b,c) {
-    //    area.styleMap.styles['default'].defaultStyle = fixmystreet.area_format;
-    //});
 }
