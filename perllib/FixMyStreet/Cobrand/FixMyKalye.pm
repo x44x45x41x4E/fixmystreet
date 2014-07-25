c804a010b3a03a1f00c3ae269a4f6b509397ff69
x44x45x41x4E <d.canlas07@gmail.com>
2014-07-25 15:51:51 +0800

    Removed reverse rendering

--- perllib/FixMyStreet/Cobrand/FixMyKalye.pm
+++ perllib/FixMyStreet/Cobrand/FixMyKalye.pm
@@ -4,8 +4,8 @@ use base 'FixMyStreet::Cobrand::Default';
 use strict;
 use warnings;
 
-# sub areas_on_around {
-#     return [ 513600, 513682 ];
-# }
+sub areas_on_around {
+    return [ 513600, 513682 ];
+}
 
 1;
