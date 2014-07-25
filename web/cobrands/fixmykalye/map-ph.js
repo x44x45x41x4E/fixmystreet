487b4484594ac181e7f0a1bc3a6f147a140fb1fa
x44x45x41x4E <d.canlas07@gmail.com>
2014-07-25 19:10:18 +0800

    Testing display boundaries

--- web/cobrands/fixmykalye/map-ph.js
+++ web/cobrands/fixmykalye/map-ph.js
@@ -12,12 +12,36 @@ function fms_ph_onload() {
     var area = new OpenLayers.Layer.Vector("KML", {
         strategies: [ new OpenLayers.Strategy.Fixed() ],
         protocol: new OpenLayers.Protocol.HTTP({
-            url: "/cobrands/phexample/ph.kml",
+            url: "/cobrands/fixmykalye/ph.kml",
             format: new OpenLayers.Format.KML()
         })
     });
+
+    var qc = new OpenLayers.Layer.Vector("KML", {
+        strategies: [ new OpenLayers.Strategy.Fixed() ],
+        protocol: new OpenLayers.Protocol.HTTP({
+            url: "/cobrands/fixmykalye/qc.kml",
+            format: new OpenLayers.Format.KML()
+        })
+    });
+
+    var mnl = new OpenLayers.Layer.Vector("KML", {
+        strategies: [ new OpenLayers.Strategy.Fixed() ],
+        protocol: new OpenLayers.Protocol.HTTP({
+            url: "/cobrands/fixmykalye/mnnl.kml",
+            format: new OpenLayers.Format.KML()
+        })
+    });
+
     area.styleMap.styles['default'].defaultStyle = fixmystreet.area_format;
-    fixmystreet.map.addLayer(area);
+
+    if ( window.location.href == "http://128.199.175.9/reports/Manila+City+Council" ) {
+        fixmystreet.map.addLayer(mnl)
+    } else if ( window.location.href == "http://128.199.175.9/reports/Quezon+City+Council" ) {
+        fixmystreet.map.addLayer(qc)
+    } else {
+        fixmystreet.map.addLayer(area);
+    }
     //area.events.register('loadend', null, function(a,b,c) {
     //    area.styleMap.styles['default'].defaultStyle = fixmystreet.area_format;
     //});
