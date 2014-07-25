1b09545895f0edba1b116833d9d0894b59ad4999
x44x45x41x4E <d.canlas07@gmail.com>
2014-07-25 09:37:56 +0800

    Debugging Javscript loader

--- web/cobrands/fixmykalye/map-ph.js
+++ web/cobrands/fixmykalye/map-ph.js
@@ -10,7 +10,6 @@ $(function(){
 
 function fms_ph_onload() {
     var extent = new OpenLayers.Bounds(120.9,14.55,0 120.9,14.78,0 121.2,14.78,0 121.2,14.55,0 120.9,14.55,0);
-    fixmystreet.map.setOptions({restrictedExtent: extent})
     var area = new OpenLayers.Layer.Vector("KML", {
         strategies: [ new OpenLayers.Strategy.Fixed() ],
         protocol: new OpenLayers.Protocol.HTTP({
@@ -20,6 +19,7 @@ function fms_ph_onload() {
     });
     area.styleMap.styles['default'].defaultStyle = fixmystreet.area_format;
     fixmystreet.map.addLayer(area);
+    fixmystreet.map.setOptions({restrictedExtent: extent})
     //area.events.register('loadend', null, function(a,b,c) {
     //    area.styleMap.styles['default'].defaultStyle = fixmystreet.area_format;
     //});
