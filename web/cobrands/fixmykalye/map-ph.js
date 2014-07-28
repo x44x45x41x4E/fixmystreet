0b43d5868a7853fea85926fcfd068c974b67d935
x44x45x41x4E <d.canlas07@gmail.com>
2014-07-29 01:20:48 +0800

    Testing..

--- web/cobrands/fixmykalye/map-ph.js
+++ web/cobrands/fixmykalye/map-ph.js
@@ -1,5 +1,4 @@
 $(function(){
-    fixmystreet.area_format = { fillColor: 'black', fillOpacity: 0.75, strokeWidth: 0, strokeColor: 'black' }
     // Vector layers must be added onload as IE sucks
     if ($.browser.msie) {
         $(window).load(fms_ph_onload);
@@ -9,14 +8,18 @@ $(function(){
 });
 
 function fms_ph_onload() {
-    var area = new OpenLayers.Layer.Vector("KML", {
+    if (fixmystreet.area.length == 0)
+    {
+        var area = new OpenLayers.Layer.Vector("KML", {
             strategies: [ new OpenLayers.Strategy.Fixed() ],
             protocol: new OpenLayers.Protocol.HTTP({
                 url: "/cobrands/fixmykalye/ph.kml",
                 format: new OpenLayers.Format.KML()
                 })
-    });
+        });
 
-    area.styleMap.styles['default'].defaultStyle;
-    fixmystreet.map.addLayer(area);
+        fixmystreet.area_format = { fillColor: 'black', fillOpacity: 0.75, strokeWidth: 0, strokeColor: 'black' }
+        area.styleMap.styles['default'].defaultStyle = fixmystreet.area_format;
+        fixmystreet.map.addLayer(area);
+    }
 }
